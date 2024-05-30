85219c2d33aa00ed3af245345972a67272712e12
Wthornton7399 <123995246+Wthornton7399@users.noreply.github.com>
2024-05-30 11:38:42 -0700

    made chainsaw a button

--- RafandWes/ContentView.swift
+++ RafandWes/ContentView.swift
@@ -12,18 +12,33 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
+    func chainsawPressed(){
+        print("chainsaw pressed")
+    }
+    
     var body: some View {
+        
+        Button(action:{chainsawPressed()}) {
+            Image("transparentChainsaw")
+                .resizable()
+                .frame(width: 400, height: 800)
+                .background(Color.red)
+    
+        }
+            
 
         ZStack{
             Color.red
                 .ignoresSafeArea()
         }
-        VStack {
-            Image("transparentChainsaw")
-                .resizable()
-                .frame(width: 400, height: 800)
-                .background(Color.red)
+        //VStack {
+            //Image("transparentChainsaw")
+                //.resizable()
+                //.frame(width: 400, height: 800)
+                //.background(Color.red)
+            
+
         }
-        .padding()
+        //.padding()
     }
-}
+//}
