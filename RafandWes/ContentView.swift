cca82183ab2b658c97411a911a7c900ec1091e00
Wthornton7399 <123995246+Wthornton7399@users.noreply.github.com>
2024-06-03 11:40:49 -0700

    Did stuff today

--- RafandWes/ContentView.swift
+++ RafandWes/ContentView.swift
@@ -9,36 +9,31 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-    
     func chainsawPressed(){
         print("chainsaw pressed")
     }
+
     
     var body: some View {
-        
         Button(action:{chainsawPressed()}) {
             Image("transparentChainsaw")
                 .resizable()
                 .frame(width: 400, height: 800)
                 .background(Color.red)
-    
         }
             
-
-        ZStack{
-            Color.red
-                .ignoresSafeArea()
-        }
-        //VStack {
-            //Image("transparentChainsaw")
-                //.resizable()
-                //.frame(width: 400, height: 800)
-                //.background(Color.red)
-            
-
-        }
-        //.padding()
+        ZStack{Color.red.ignoresSafeArea()}
+        
     }
-//}
+
+}
+
+
+
+
+
+
+
+
+
+
