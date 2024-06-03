9ef9317c3dd9cc21f709124f8b4ca7fe824b6cd3
rcruikshank2744 <rcruikshank2744@student.pps.net>
2024-06-03 11:01:23 -0700

    sound

--- RafandWes/ContentView.swift
+++ RafandWes/ContentView.swift
@@ -7,17 +7,26 @@
 
 import SwiftUI
 import SwiftData
+import AVFoundation
+
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
+    @State var player: AVAudioPlayer?
     func chainsawPressed(){
         print("chainsaw pressed")
+        self.playSound()
     }
-    
+    func playSound(){
+        let url = Bundle.main.url(forResource: "chainsaw-92374", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+    }
     var body: some View {
         
-        Button(action:{chainsawPressed()}) {
+    Button(action:{chainsawPressed()}) {
             Image("transparentChainsaw")
                 .resizable()
                 .frame(width: 400, height: 800)
