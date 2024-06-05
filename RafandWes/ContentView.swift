c86941c0cf116a1a7144ad63fb61c5a6761d929a
rcruikshank2744 <rcruikshank2744@student.pps.net>
2024-06-05 11:37:38 -0700

    sounds manager

--- RafandWes/ContentView.swift
+++ RafandWes/ContentView.swift
@@ -14,28 +14,33 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
     
-    @State var player: AVAudioPlayer?
-    func chainsawPressed(){
-        print("chainsaw pressed")
-        self.playSound()
-    }
-    func playSound(){
-        let url = Bundle.main.url(forResource: "chainsawOne", withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player!.play()
-        
-    }
+
+    @State var longPressGesture = false
     var body: some View {
+        ZStack{
+            Button(action:{
+                longPressGesture.toggle()
+            }) {
+                Image("transparentChainsaw")
+                    .resizable()
+                    .frame(width: 400, height: 800)
+                    .background(Color.red)
+            }
+            .simultaneousGesture(LongPressGesture(minimumDuration: 0.1)
+                .onEnded{ _ in
+                    print("long press ended")
+                    longPressGesture.toggle()
+                })
+                    .onChange(of: longPressGesture, perform:
+                                {value in
+                        if longPressGesture {
+                            playSound(sound: "chainsawOne", type: "wav")
+                        } else {
+                            stopSound(sound: "chainsawOne", type: "wav")
+                        }
+                    })
         
-        Button(action:{chainsawPressed()}) {
-            Image("transparentChainsaw")
-                .resizable()
-                .frame(width: 400, height: 800)
-                .background(Color.red)
-                .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-    
         }
-            
 
         ZStack{
             Color.red
---
+++ RafandWes/Sounds.swift
@@ -0,0 +1,41 @@
+//
+//  Sounds.swift
+//  RafandWes
+//
+//  Created by Rafferty Cruikshank on 6/5/24.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.numberOfLoops = -1
+            audioPlayer?.play()
+            
+            print("sound file played successfully")
+        } catch {
+            print("could not play sound")
+        }
+    }
+}
+func stopSound(sound: String, type: String) {
+    
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.stop()
+            
+            print("sound file stopped successfully")
+        } catch {
+            print("could not play stop sound")
+        }
+    }
+}
