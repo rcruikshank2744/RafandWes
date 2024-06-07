4cef3dc22bea2889dd15c743a00a4387082ece25
Wthornton7399 <123995246+Wthornton7399@users.noreply.github.com>
2024-06-07 11:59:01 -0700

    made code formatting easier to read, deleted unnecessary code/files
    made code formatting easier to read
    
    deleted unnecessary code/files
    
    added a new chainsaw noise
    
    made RNG to allow for both noises to be played at random
    
    made the if statements to allow for the noises to be played when clicked
    
    cleaned up formatting

--- RafandWes/ContentView.swift
+++ RafandWes/ContentView.swift
@@ -13,44 +13,46 @@ import AVFoundation
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-    
-    
     @State var longPressGesture = false
+    let soundSelect = Int.random(in: 0..<2)
+
     var body: some View {
-        ZStack{
-            Button(action:{
-                longPressGesture.toggle()
-            }) {
+        
+        ZStack {
+            
+            Button(action:{longPressGesture.toggle()}) {
                 Image("transparentChainsaw")
                     .resizable()
                     .frame(width: 400, height: 800)
                     .background(Color.red)
             }
+            
             .simultaneousGesture(LongPressGesture(minimumDuration: 0.1)
                 .onEnded{ _ in
                     print("long press ended")
                     longPressGesture.toggle()
-                })
-            .onChange(of: longPressGesture, perform:
-                        {value in
-                if longPressGesture {
-                    playSound(sound: "chainsawOne", type: "wav")
-                } else {
-                    stopSound(sound: "chainsawOne", type: "wav")
+            })
+            
+            .onChange(of: longPressGesture, perform: {
+                value in
+                print(soundSelect)
+                
+                if(soundSelect == 0){
+                    if longPressGesture {playSound(sound: "chainsawOne", type: "wav")}
+                    else {stopSound(sound: "chainsawOne", type: "wav")}
                 }
+                
+                else if(soundSelect == 1){
+                    if longPressGesture {playSound(sound: "chainsawTwo", type: "wav")}
+                    else {stopSound(sound: "chainsawTwo", type: "wav")}
+                }
+                
+                else{print("no sound played")}
+                
             })
             
         }
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
